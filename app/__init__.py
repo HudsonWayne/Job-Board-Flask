61cdb56f1c041edf9c6d00d38acf33993afe3a9e
HudsonWayne <waynewaynebenhura@gmail.com>
2025-04-30 13:36:53 +0200

    added my home.html

--- app/__init__.py
+++ app/__init__.py
@@ -1,22 +1,28 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 from flask_migrate import Migrate
 
+# Create extension instances
 db = SQLAlchemy()
+login_manager = LoginManager()
 migrate = Migrate()
-
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'HudsonWayne'
+    
+    # Config settings
+    app.config['SECRET_KEY'] = 'HudsonWayne'  
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///jobboard.db'
-    
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    # Initialize extensions
     db.init_app(app)
+    login_manager.init_app(app)
     migrate.init_app(app, db)
-    from .routes import main
-    app.register_blueprint(main)
-    
+
+    # Import and register blueprints
     from jobboard.routes.auth import auth
     app.register_blueprint(auth)
-    
+
     return app
