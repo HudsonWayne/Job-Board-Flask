4f54a4f06115b0f1713c947a9618d09c3dd1fc88
HudsonWayne <waynewaynebenhura@gmail.com>
2025-05-02 13:43:06 +0200

    added the job routes

--- app/routes/job.py
+++ app/routes/job.py
@@ -2,3 +2,12 @@ from flask import Blueprint, render_template, redirect, url_for, request, flash
 from flask_login import login_required, current_user
 from app import db
 from app.models import Job
+
+
+job = Blueprint('job', __name__)
+
+
+@job.route('/jobs')
+def list_jobs():
+    jobs = Job.query.all()
+    return render_template('jobs.html', jobs=jobs)
