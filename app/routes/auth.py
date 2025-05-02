3e088feb88f9f0b8cc352ff4cbca1bd464101900
HudsonWayne <waynewaynebenhura@gmail.com>
2025-05-02 11:50:16 +0200

    imported my modules on the auth.py

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -1,4 +1,8 @@
-from flask import Blueprint,render_template
+from flask import Blueprint, render_template, redirect, url_for, flash, request
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import login_user, logout_user, login_required
+from app import db
+from app.models import User
 
 
 auth = Blueprint('auth',__name__)
