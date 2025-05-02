14ef4df2a40a5222401b2af936751a5db6b2e923
HudsonWayne <waynewaynebenhura@gmail.com>
2025-05-02 11:27:47 +0200

    updated the user

--- app/models/user.py
+++ app/models/user.py
@@ -1,7 +1,10 @@
 from .. import db  # Relative import to avoid circular import
 from datetime import datetime
+from flask_login import UserMixin
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(200), nullable=False)
+    is_employer = db.Column(db.Boolean, default=False)
+    jobs = db.relationship('JobPost', backref='employer', lazy=True)
