3d019f3fd154502ec42660e57c2271c6be4bdc6b
HudsonWayne <waynewaynebenhura@gmail.com>
2025-05-05 11:12:43 +0200

    added company locarion and field

--- app/forms.py
+++ app/forms.py
@@ -4,4 +4,6 @@ from wtforms.validators import DataRequired, Length
 
 
 class JobForm(FlaskForm):
-    title = StringField('Job Title')
+    title = StringField('Job Title', validators=[DataRequired(), Length(max=100)])
+    company = StringField('Company Name', validators=[DataRequired(), Length(max=100)])
+    location = StringField('Location', validators=[DataRequired(), Length(max=100)])
