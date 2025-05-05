c401d3d66364aacdacd72424c272b6a4d2c015bb
HudsonWayne <waynewaynebenhura@gmail.com>
2025-05-05 11:11:54 +0200

    added the job form

--- app/forms.py
+++ app/forms.py
@@ -1,3 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField, SubmitField
 from wtforms.validators import DataRequired, Length
+
+
+class JobForm(FlaskForm):
+    title = StringField('Job Title')
